ce79a1333c82dbac43a5c5e0cb329a3b82414491
Criss7w7 <cristina050917@gmail.com>
2022-03-10 09:41:34 -0600

    Estructura de proyecto

---
+++ src/developers/MiPrimeraAplicacionJava.java
@@ -0,0 +1,31 @@
+package developers;
+
+public class MiPrimeraAplicacionJava {
+    public static void main(String[] args){
+        String nombreCompleto, direccion, estadoCivil;
+        int edad;
+        double gastosDiarios;
+        char genero;
+        boolean poseeVehiculo;
+        
+        //Inicializacio  de variables
+        nombreCompleto = "Camila Esperanza Rosales";
+        direccion = "Calle de la armagura desvio la tristeza";
+        estadoCivil = "Soltera";
+        edad = 30;
+        gastosDiarios = 34.45;
+        genero = 'F';
+        poseeVehiculo = false;
+        
+        System.out.println("Nombre completo:" + nombreCompleto);
+        System.out.println("Direccion      :" + direccion);
+        System.out.println("Estado Civil   :" + estadoCivil);
+        System.out.println("Edad           :" + edad);
+        System.out.println("Gastos diarios :" + gastosDiarios);
+        System.out.println("Genero         :" + genero);
+        System.out.println("Posee Vehiculo :" + poseeVehiculo);
+        
+       
+    }
+    
+}
