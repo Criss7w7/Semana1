eb0c4c8918bb1c973b07b3ed508ade3d8e92fff7
Criss7w7 <cristina050917@gmail.com>
2022-03-10 11:39:43 -0600

    Clase Java de calculo

---
+++ src/developers/CalculoInteresSimple.java
@@ -0,0 +1,8 @@
+package developers;
+public class CalculoInteresSimple {
+    public static void main(String[] args){
+        //Declara variables
+        double interes, capitalPrestado, tiempo, tasaInteres, tiempoEnMeses;
+        
+}
+}
